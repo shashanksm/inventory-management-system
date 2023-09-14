89bc8df3b1d51c1f15cc63cc01016f1fe023a68d
shashanksm <shashank.mutgi@gmail.com>
2023-09-14 18:43:17 +0530

    first commit

---
+++ src/test/java/io/shashanksm/ims/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shashanksm.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
